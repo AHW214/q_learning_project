1a4bf49c6682f735015467cd894992825ce4f616
Adam Weider <adamweider98@gmail.com>
2021-02-22 02:47:55 -0600

    skeleton

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -1,40 +1,114 @@
 #!/usr/bin/env python3
 
-import rospy
+# pyright: reportMissingTypeStubs=false
 
-#from gazebo_msgs.msg import ModelState, ModelStates
-#from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
-#import time
-#from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from dataclasses import dataclass, replace
+from typing import Any, List, Tuple
+
+import rospy
+from rospy_util.controller import Cmd, Controller, Sub
+
+import controller.cmd as cmd
+import controller.sub as sub
+from q_learning_project.msg import RobotMoveDBToBlock, QLearningReward
+
+### Model ###
+
+
+@dataclass
+class Model:
+    actions: List[RobotMoveDBToBlock]
+    rewards: List[QLearningReward]
+
+
+init_model: Model = Model(
+    actions=[
+        RobotMoveDBToBlock(robot_db="red", block_id=1),
+        RobotMoveDBToBlock(robot_db="green", block_id=2),
+        RobotMoveDBToBlock(robot_db="blue", block_id=3),
+    ],
+    rewards=[],
+)
+
+
+### Update ###
+
+
+def update(reward: QLearningReward, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
+    rewards = [reward, *model.rewards]
+
+    print(rewards)
+
+    if not model.actions:
+        return (replace(model, rewards=rewards), [])
+
+    (action, *rest) = model.actions
+    return (
+        Model(actions=rest, rewards=rewards),
+        [cmd.robot_action(action)],
+    )
+
+
+### Subscriptions ###
+
+
+def subscriptions(_: Model) -> List[Sub[Any, QLearningReward]]:
+    return [
+        sub.q_learning_reward(lambda r: r),
+    ]
+
+
+### Run ###
+
+
+def run() -> None:
+    rospy.init_node("q_learning")
+
+    Controller.run(
+        model=init_model,
+        update=update,
+        subscriptions=subscriptions,
+    )
+
+    rospy.spin()
+
+
+# from gazebo_msgs.msg import ModelState, ModelStates
+# from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+# import time
+# from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from q_learning_project.msg import RobotMoveDBToBlock, QMatrix, QLearningReward
 
 
 class Q_Learning(object):
-
     def __init__(self):
 
         # initialize this node
-        rospy.init_node('turtlebot3_q_learning')
+        rospy.init_node("turtlebot3_q_learning")
 
         self.robot_dbs = ["red", "green", "blue"]
         # db model names
         self.db_model_names = {
             "red": "robot_dumbbell_red",
             "green": "robot_dumbbell_green",
-            "blue": "robot_dumbbell_blue"
+            "blue": "robot_dumbbell_blue",
         }
 
-        # Create a publisher to the qmatrix 
+        # Create a publisher to the qmatrix
         print("publisher for qmatrix...")
-        self.q_matrix_pub = rospy.Publisher("/q_learning/QMatix", QMatrix, queue_size=10)
+        self.q_matrix_pub = rospy.Publisher(
+            "/q_learning/QMatix", QMatrix, queue_size=10
+        )
 
         # Create subscriber for reward of message type QLearningReward
         print("subscriber for reward...")
         rospy.Subscriber("/q_learning/reward", QLearningReward, self.reward_received)
-        
+
         # Create a publisher for robot actions
         print("publisher for robot_action...")
-        self.robot_action_pub = rospy.Publisher("/q_learning/robot_action", RobotMoveDBToBlock, queue_size=10)
+        self.robot_action_pub = rospy.Publisher(
+            "/q_learning/robot_action", RobotMoveDBToBlock, queue_size=10
+        )
 
     def reward_received(data):
         reward = data.reward
@@ -47,7 +121,7 @@ class Q_Learning(object):
         while not ctrl_c:
             connections = self.robot_action_pub.get_num_connections()
             if connections > 0:
-                for k in range(1,10):
+                for k in range(1, 10):
                     robot_action_data = RobotMoveDBToBlock()
                     robot_action_data.robot_db = self.robot_dbs[k % 3]
                     robot_action_data.block_id = 1 + k % 3
@@ -59,7 +133,7 @@ class Q_Learning(object):
         rospy.spin()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     node = Q_Learning()
     node.run()
